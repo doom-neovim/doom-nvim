5aaf05559e61f437ef9aa875ebc0d72dee95ef4f
NTBBloodbath <captainspark365@gmail.com>
2021-04-29 00:04:39 -0400

    plugins: update toggleterm config

--- lua/plugins/configs/nvim-toggleterm.lua
+++ lua/plugins/configs/nvim-toggleterm.lua
@@ -1,9 +1,20 @@
 require('toggleterm').setup {
-    size = 10,
+    size = g.doom_terminal_height,
     open_mapping = [[<c-t>]],
+    hide_numbers = true,
     shade_filetypes = {},
     shade_terminals = true,
     start_in_insert = true,
     persist_size = true,
-    direction = 'horizontal'
+    direction = g.doom_terminal_direction,
+    float_opts = {
+        border = 'curved',
+        width = g.doom_terminal_width,
+        height = g.doom_terminal_height,
+        winblend = 0,
+        highlights = {
+            border = 'Special',
+            background = 'Normal',
+        }
+    }
 }
