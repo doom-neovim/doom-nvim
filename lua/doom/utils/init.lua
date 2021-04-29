30e8fab6a0e3872aee078d22ca2d94c8e79197f6
NTBBloodbath <captainspark365@gmail.com>
2021-04-29 19:58:01 -0400

    utils: remove unused function

--- lua/doom/utils/init.lua
+++ lua/doom/utils/init.lua
@@ -21,20 +21,6 @@ function opt(scope, key, value)
     if scope ~= 'o' then scopes['o'][key] = value end
 end
 
--- For autocommands, extracted from
--- https://github.com/norcalli/nvim_utils
-function nvim_create_augroups(definitions)
-    for group_name, definition in pairs(definitions) do
-        execute('augroup ' .. group_name)
-        execute('autocmd!')
-        for _, def in ipairs(definition) do
-            local command = table.concat(vim.tbl_flatten {'autocmd', def}, ' ')
-            execute(command)
-        end
-        execute('augroup END')
-    end
-end
-
 -- Check if string is empty or if it's nil
 function is_empty(str) return str == '' or str == nil end
 
