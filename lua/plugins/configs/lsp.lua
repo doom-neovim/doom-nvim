421f44628076ad5b5e97d5fff5c4e3cb386457b2
Gustavo Prieto <vgdvx17@gmail.com>
2021-05-06 15:53:03 -0400

    Changed personal settings

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -68,40 +68,87 @@ local on_attach = function(client, bufnr)
 	buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 	-- Mappings.
 	local opts = { silent = true }
-    Map("n", "<leader>gt", "<cmd>LspTroubleToggle<cr>",
-      opts
-    )
-    Map("n", "<leader>xw", "<cmd>LspTroubleToggle lsp_workspace_diagnostics<cr>",
-      opts
-    )
-    Map("n", "<leader>xd", "<cmd>LspTroubleToggle lsp_document_diagnostics<cr>",
-      opts
-    )
-    Map("n", "<leader>xl", "<cmd>LspTroubleToggle loclist<cr>",
-      opts
-    )
-    Map("n", "<leader>xq", "<cmd>LspTroubleToggle quickfix<cr>",
-      opts
-    )
-    Map("n", "gr", "<cmd>LspTrouble lsp_references<cr>",
-      opts
-    )
-	-- Set some keybinds conditional on server capabilities
-	if client.resolved_capabilities.document_formatting then
-		Map(
-			'n',
-			'<space>f',
-			'<cmd>lua vim.lsp.buf.formatting()<CR>',
-			opts
-		)
-	elseif client.resolved_capabilities.document_range_formatting then
-		Map(
-			'n',
-			'<space>f',
-			'<cmd>lua vim.lsp.buf.range_formatting()<CR>',
-			opts
-		)
-	end
+	Map(
+		'n',
+		'ga',
+		'<Cmd>lua vim.lsp.buf.code_action()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'gD',
+		'<Cmd>lua vim.lsp.buf.declaration()<CR>',
+		opts
+	)
+	Map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	Map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	Map(
+		'n',
+		'gi',
+		'<cmd>lua vim.lsp.buf.implementation()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'<C-k>',
+		'<cmd>lua vim.lsp.buf.signature_help()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'<space>wa',
+		'<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'<space>wr',
+		'<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'<space>wl',
+		'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'<space>D',
+		'<cmd>lua vim.lsp.buf.type_definition()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'<space>rn',
+		'<cmd>lua vim.lsp.buf.rename()<CR>',
+		opts
+	)
+	Map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	Map(
+		'n',
+		'<space>e',
+		'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'[d',
+		'<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		']d',
+		'<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
+		opts
+	)
+	Map(
+		'n',
+		'<space>q',
+		'<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
+		opts
+	)
 	-- Set autocommands conditional on server_capabilities
 	if client.resolved_capabilities.document_highlight then
 		Api.nvim_exec(
