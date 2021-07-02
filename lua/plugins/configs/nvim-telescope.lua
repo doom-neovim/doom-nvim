8aaaa6f754121115cd93ef2d7c6f5c120a760e71
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-07-02 01:11:54 -0400

    hotfix(telescope): fix breaking changes, closes #25

--- lua/plugins/configs/nvim-telescope.lua
+++ lua/plugins/configs/nvim-telescope.lua
@@ -6,24 +6,24 @@ local telescope = require('telescope')
 telescope.setup({
 	defaults = {
         find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
-		prompt_position = 'bottom',
-		prompt_prefix = '>',
+		prompt_prefix = '> ',
 		initial_mode = 'insert',
 		selection_strategy = 'reset',
 		sorting_strategy = 'descending',
 		layout_strategy = 'horizontal',
-		layout_defaults = {
-			-- TODO add builtin options.
+		layout_config = {
+		    prompt_position = 'bottom',
+			horizontal = {
+			    mirror = false,
+			    preview_width = 0.6,
+			},
 		},
 		file_sorter = require('telescope.sorters').get_fuzzy_file,
 		file_ignore_patterns = {},
 		generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
 		shorten_path = true,
 		winblend = 0,
-		width = 0.75,
-		preview_cutoff = 80,
-		results_height = 1,
-		results_width = 0.8,
+		scroll_strategy = 'cycle',
 		border = {},
 		borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
 		color_devicons = true,
