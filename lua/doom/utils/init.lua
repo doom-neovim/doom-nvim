e0c9eb0cb80e07d2b405bdb3fdcaa25392a2d6fe
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-07-06 19:46:11 -0400

    feat(utils): add read/write Luv functions

--- lua/doom/utils/init.lua
+++ lua/doom/utils/init.lua
@@ -76,4 +76,32 @@ M.get_os = function()
 	return jit.os
 end
 
+-- read_file returns the content of the given file
+-- @tparam string path The path of the file
+-- @return string
+M.read_file = function(path)
+	local fd = vim.loop.fs_open(path, 'r', 438)
+	local stat = vim.loop.fs_fstat(fd)
+	local data = vim.loop.fs_read(fd, stat.size, 0)
+	vim.loop.fs_close(fd)
+
+	return data
+end
+
+-- write_file writes the given string into given file
+-- @tparam string path The path of the file
+-- @tparam string content The content to be written in the file
+-- @tparam string mode The mode for opening the file, e.g. 'w+'
+M.write_file = function(path, content, mode)
+    -- 644 sets read and write permissions for the owner, and it sets read-only
+    -- mode for the group and others.
+    vim.loop.fs_open(path, mode, tonumber('644', 8), function(err, fd)
+        if not err then
+            local fpipe = vim.loop.new_pipe(false)
+            vim.loop.pipe_open(fpipe, fd)
+            vim.loop.write(fpipe, content)
+        end
+    end)
+end
+
 return M
