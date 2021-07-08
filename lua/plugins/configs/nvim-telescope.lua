0c117a0fa8296b9abec6a074d782df2ac5f542b8
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-07-08 13:55:36 -0400

    fix(telescope): adapt current configs to breaking changes

--- lua/plugins/configs/nvim-telescope.lua
+++ lua/plugins/configs/nvim-telescope.lua
@@ -21,7 +21,7 @@ telescope.setup({
 		file_sorter = require('telescope.sorters').get_fuzzy_file,
 		file_ignore_patterns = {},
 		generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
-		shorten_path = true,
+		path_display = 'shorten',
 		winblend = 0,
 		scroll_strategy = 'cycle',
 		border = {},
