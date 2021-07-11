57e799ff20da8641d08b9eb0206809bba2c90fcb
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-07-10 20:00:22 -0400

    fix(telescope): revert path_display changes

--- lua/doom/modules/config/doom-telescope.lua
+++ lua/doom/modules/config/doom-telescope.lua
@@ -29,7 +29,6 @@ return function()
 			file_sorter = require('telescope.sorters').get_fuzzy_file,
 			file_ignore_patterns = {},
 			generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
-			path_display = 'shorten',
 			winblend = 0,
 			scroll_strategy = 'cycle',
 			border = {},
