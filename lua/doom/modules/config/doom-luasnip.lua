7b4b1fbab596e948e75c38b88ef54644620bcbc0
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-07-13 14:57:11 -0400

    fix:   - Add luasnip to compe sources   - Clean unneeded code in luasnip's config   - Add neorg to compe sources

--- lua/doom/modules/config/doom-luasnip.lua
+++ lua/doom/modules/config/doom-luasnip.lua
@@ -1,7 +1,3 @@
 return function()
-	require('luasnip.loaders.from_vscode').lazy_load({
-		paths = {
-			vim.fn.stdpath('data') .. '/site/pack/packer/opt/friendly-snippets',
-		},
-	})
+	require('luasnip.loaders.from_vscode').lazy_load()
 end
