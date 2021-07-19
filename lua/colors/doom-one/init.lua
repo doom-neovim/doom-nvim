4dea8d3baba5f381a41ffa9ca697cf0b44b03b24
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-07-19 15:59:16 -0400

    feat(doom-one): pull upstream changes

--- lua/colors/doom-one/init.lua
+++ lua/colors/doom-one/init.lua
@@ -1,9 +1,9 @@
 -- vim:fdm=marker
 -- Vim Color File
--- Name:       doom-one
--- Maintainer: http//github.com/NTBBloodbath
--- License:    The MIT License (MIT)
--- Based On:   http//github.com/romgrk/doom-one.vim and the original Doom Emacs one
+-- Name:            doom-one
+-- Maintainer:      http//github.com/NTBBloodbath
+-- License:         The MIT License (MIT)
+-- Based On:        http//github.com/romgrk/doom-one.vim and the original Doom Emacs one
 
 -- Helpers {{{
 
@@ -233,11 +233,11 @@ local general_ui = {
 
 	TabLine = {
 		fg = base7,
-		bg = transparent_bg and 'NONE' or bg_alt,
+		bg = bg_alt,
 		gui = 'bold',
 	},
 	TabLineSel = { fg = blue, bg = bg, gui = 'bold' },
-	TabLineFill = { bg = transparent_bg and 'NONE' or base1, gui = 'bold' },
+	TabLineFill = { bg = base1, gui = 'bold' },
 
 	StatusLine = { fg = base8, bg = base3 },
 	StatusLineNC = { fg = base6, bg = bg_popup },
@@ -254,8 +254,6 @@ local general_ui = {
 if vim.opt.pumblend == 1 then
 	vim.opt.pumblend = 20
 end
-
-apply_highlight(general_ui)
 
 -- }}}
 
@@ -285,45 +283,6 @@ local search_high_ui = {
 	Bold = { gui = 'bold' },
 	Emphasis = { fg = green, gui = 'bold' },
 }
-
-apply_highlight(search_high_ui)
-
--- }}}
-
--- Text levels {{{
-
-local text_colors = {
-	Normal = fg,
-	Info = blue,
-	Success = green,
-	Warning = yellow,
-	Debug = yellow,
-	Error = red,
-	Special = violet,
-	Muted = base7,
-}
-
-for key, _ in pairs(text_colors) do
-	apply_highlight({
-		['Text' .. key] = {
-			fg = text_colors[key],
-		},
-	})
-	apply_highlight({
-		['Text' .. key .. 'Bold'] = {
-			fg = text_colors[key],
-			gui = 'bold',
-		},
-	})
-end
-
-high_link('Msg', 'TextSuccess')
-high_link('MoreMsg', 'TextInfo')
-high_link('WarningMsg', 'TextWarning')
-high_link('Error', 'TextError')
-high_link('ErrorMsg', 'TextError')
-high_link('ModeMsg', 'TextSpecial')
-high_link('Todo', 'TextWarningBold')
 
 -- }}}
 
@@ -398,12 +357,6 @@ local main_syntax = {
 	RegexpKey = { fg = '#5f0041', gui = 'bold' },
 }
 
-apply_highlight(main_syntax)
-high_link('CommentURL', 'URL')
-high_link('CommentLabel', 'CommentBold')
-high_link('CommentSection', 'CommentBold')
-high_link('Noise', 'Comment')
-
 -- }}}
 
 -- Diff {{{
@@ -434,11 +387,11 @@ high_clear('DiffAdd')
 high_clear('DiffChange')
 high_clear('DiffText')
 high_clear('DiffDelete')
-apply_highlight(diff)
 
 -- }}}
 
 -- Markdown {{{
+
 local markdown = {
 	markdownCode = { bg = bg_highlight },
 	markdownCodeBlock = { bg = bg_highlight },
@@ -447,7 +400,6 @@ local markdown = {
 	markdownLinkText = { gui = 'underline' },
 }
 
-apply_highlight(markdown)
 --}}}
 
 -- Plugins {{{
@@ -479,37 +431,13 @@ local barbar = {
 	BufferPart = { fg = diff_info_fg, bg = diff_info_bg0, gui = 'bold' },
 }
 
-apply_highlight(barbar)
-
--- }}}
-
--- Gitgutter {{{
-
-high_link('GitGutterAdd', 'DiffAddedGutter')
-high_link('GitGutterChange', 'DiffModifiedGutter')
-high_link('GitGutterDelete', 'DiffRemovedGutter')
-high_link('GitGutterChangeDelete', 'DiffModifiedGutter')
-
-high_link('GitGutterAddLineNr', 'DiffAddedGutterLineNr')
-high_link('GitGutterChangeLineNr', 'DiffModifiedGutterLineNr')
-high_link('GitGutterDeleteLineNr', 'DiffRemovedGutterLineNr')
-high_link('GitGutterChangeDeleteLineNr', 'DiffModifiedGutterLineNr')
-
--- }}}
-
--- Gitsigns {{{
-
-high_link('GitSignsAdd', 'DiffAddedGutter')
-high_link('GitSignsChange', 'DiffModifiedGutter')
-high_link('GitSignsDelete', 'DiffRemovedGutter')
-high_link('GitSignsChangeDelete', 'DiffModifiedGutter')
-
 -- }}}
 
 -- Telescope {{{
 
+local telescope = {}
 if vim.g.doom_one_telescope_highlights then
-	local telescope = {
+	telescope = {
 		TelescopeSelection = { fg = yellow, gui = 'bold' },
 		TelescopeSelectionCaret = { fg = light_bg and orange or blue },
 		TelescopeMultiSelection = { fg = grey },
@@ -521,9 +449,6 @@ if vim.g.doom_one_telescope_highlights then
 		TelescopeResultsBorder = { fg = light_bg and orange or blue },
 		TelescopePreviewBorder = { fg = light_bg and orange or blue },
 	}
-
-	apply_highlight(telescope)
-	high_link('TelescopePrompt', 'TelescopeNormal')
 end
 
 -- }}}
@@ -531,6 +456,8 @@ end
 -- NvimTree {{{
 
 local nvim_tree = {
+	NvimTreeNormal = { bg = bg_alt },
+	NvimTreeEndOfBuffer = { fg = bg_alt, bg = bg_alt },
 	NvimTreeFolderName = { fg = light_bg and base9 or blue, gui = 'bold' },
 	NvimTreeRootFolder = { fg = green },
 	NvimTreeEmptyFolderName = { fg = fg_alt, gui = 'bold' },
@@ -542,17 +469,6 @@ local nvim_tree = {
 	NvimTreeMarkdownFile = { fg = fg, gui = 'underline' },
 }
 
-apply_highlight(nvim_tree)
-high_link('NvimTreeGitDirty', 'DiffModifiedGutter')
-high_link('NvimTreeGitStaged', 'DiffModifiedGutter')
-high_link('NvimTreeGitMerge', 'DiffModifiedGutter')
-high_link('NvimTreeGitRenamed', 'DiffModifiedGutter')
-high_link('NvimTreeGitNew', 'DiffAddedGutter')
-high_link('NvimTreeGitDeleted', 'DiffRemovedGutter')
-
-high_link('NvimTreeIndentMarker', 'IndentGuide')
-high_link('NvimTreeOpenedFolderName', 'NvimTreeFolderName')
-
 -- }}}
 
 -- Dashboard {{{
@@ -562,9 +478,6 @@ local dashboard = {
 	dashboardCenter = { fg = light_bg and orange or blue },
 	dashboardShortcut = { fg = '#9788b9' },
 }
-
-apply_highlight(dashboard)
-high_link('dashboardFooter', 'dashboardHeader')
 
 -- }}}
 
@@ -579,7 +492,32 @@ local whichkey = {
 	WhichKeyValue = { fg = grey },
 }
 
-apply_highlight(whichkey)
+-- }}}
+
+-- indent-blankline {{{
+
+local indent_blankline = {
+	IndentBlanklineChar = {
+		fg = base4,
+		cterm = 'nocombine',
+		gui = 'nocombine',
+	},
+	IndentBlanklineContextChar = {
+		fg = blue,
+		cterm = 'nocombine',
+		gui = 'nocombine',
+	},
+	IndentBlanklineSpaceChar = {
+		fg = base4,
+		cterm = 'nocombine',
+		gui = 'nocombine',
+	},
+	IndentBlanklineSpaceCharBlankline = {
+		fg = base4,
+		cterm = 'nocombine',
+		gui = 'nocombine',
+	},
+}
 
 -- }}}
 
@@ -594,35 +532,6 @@ local msg_underline = {
 	MsgUnderline = { fg = green, gui = 'undercurl' },
 }
 
-apply_highlight(msg_underline)
-high_link('LspDiagnosticsFloatingError', 'ErrorMsg')
-high_link('LspDiagnosticsFloatingWarning', 'Warning')
-high_link('LspDiagnosticsFloatingInformation', 'MoreMsg')
-high_link('LspDiagnosticsFloatingHint', 'Msg')
-high_link('LspDiagnosticsDefaultError', 'ErrorMsg')
-high_link('LspDiagnosticsDefaultWarning', 'WarningMsg')
-high_link('LspDiagnosticsDefaultInformation', 'MoreMsg')
-high_link('LspDiagnosticsDefaultHint', 'Msg')
-high_link('LspDiagnosticsVirtualTextError', 'ErrorMsg')
-high_link('LspDiagnosticsVirtualTextWarning', 'WarningMsg')
-high_link('LspDiagnosticsVirtualTextInformation', 'MoreMsg')
-high_link('LspDiagnosticsVirtualTextHint', 'Msg')
-high_link('LspDiagnosticsUnderlineError', 'ErrorMsgUnderline')
-high_link('LspDiagnosticsUnderlineWarning', 'WarningMsgUnderline')
-high_link('LspDiagnosticsUnderlineInformation', 'MoreMsgUnderline')
-high_link('LspDiagnosticsUnderlineHint', 'MsgUnderline')
-high_link('LspDiagnosticsSignError', 'ErrorMsg')
-high_link('LspDiagnosticsSignWarning', 'WarningMsg')
-high_link('LspDiagnosticsSignInformation', 'MoreMsg')
-high_link('LspDiagnosticsSignHint', 'Msg')
-high_link('LspReferenceText', 'Bold')
-high_link('LspReferenceRead', 'Bold')
-high_link('LspReferenceWrite', 'Bold')
-high_link('TermCursor', 'Cursor')
-high_link('healthError', 'ErrorMsg')
-high_link('healthSuccess', 'Msg')
-high_link('healthWarning', 'WarningMsg')
-
 -- LspSaga {{{
 
 local lspsaga = {
@@ -630,114 +539,290 @@ local lspsaga = {
 	LspSagaDiagnosticHeader = { fg = red },
 }
 
-apply_highlight(lspsaga)
-high_link('LspSagaDiagnosticBorder', 'Normal')
-high_link('LspSagaDiagnosticTruncateLine', 'Normal')
-high_link('LspFloatWinBorder', 'Normal')
-high_link('LspSagaBorderTitle', 'Title')
-high_link('TargetWord', 'Error')
-high_link('ReferencesCount', 'Title')
-high_link('ReferencesIcon', 'Special')
-high_link('DefinitionCount', 'Title')
-high_link('TargetFileName', 'Comment')
-high_link('DefinitionIcon', 'Special')
-high_link('ProviderTruncateLine', 'Normal')
-high_link('LspSagaFinderSelection', 'Search')
-high_link('DiagnosticTruncateLine', 'Normal')
-high_link('DiagnosticError', 'LspDiagnosticsDefaultError')
-high_link('DiagnosticWarning', 'LspDiagnosticsDefaultWarning')
-high_link('DiagnosticInformation', 'LspDiagnosticsDefaultInformation')
-high_link('DiagnosticHint', 'LspDiagnosticsDefaultHint')
-high_link('DefinitionPreviewTitle', 'Title')
-high_link('LspSagaShTruncateLine', 'Normal')
-high_link('LspSagaDocTruncateLine', 'Normal')
-high_link('LineDiagTuncateLine', 'Normal')
-high_link('LspSagaCodeActionTitle', 'Title')
-high_link('LspSagaCodeActionTruncateLine', 'Normal')
-high_link('LspSagaCodeActionContent', 'Normal')
-high_link('LspSagaRenamePromptPrefix', 'Normal')
-high_link('LspSagaRenameBorder', 'Bold')
-high_link('LspSagaHoverBorder', 'Bold')
-high_link('LspSagaSignatureHelpBorder', 'Bold')
-high_link('LspSagaCodeActionBorder', 'Bold')
-high_link('LspSagaDefPreviewBorder', 'Bold')
-high_link('LspLinesDiagBorder', 'Bold')
-
 -- }}}
 
 -- }}}
 
--- TreeSitter {{{
+-- Load colorscheme asynchronously {{{
 
-if vim.g.doom_one_enable_treesitter then
-	high_link('TSException', 'Exception')
-	high_link('TSAnnotation', 'PreProc')
-	high_link('TSAttribute', 'Attribute')
-	high_link('TSConditional', 'Conditional')
-	high_link('TSComment', 'Comment')
-	high_link('TSConstructor', 'Structure')
-	high_link('TSConstant', 'Constant')
-	high_link('TSConstBuiltin', 'Constant')
-	high_link('TSConstMacro', 'Macro')
-	high_link('TSError', 'Error')
-	high_link('TSField', 'Field')
-	high_link('TSFloat', 'Float')
-	high_link('TSFunction', 'Function')
-	high_link('TSFuncBuiltin', 'FunctionBuiltin')
-	high_link('TSFuncMacro', 'Macro')
-	high_link('TSInclude', 'Include')
-	high_link('TSKeyword', 'Keyword')
-	high_link('TSKeywordFunction', 'KeywordFunction')
-	high_link('TSLabel', 'Label')
-	high_link('TSMethod', 'Method')
-	high_link('TSNamespace', 'Directory')
-	high_link('TSNumber', 'Number')
-	high_link('TSOperator', 'Operator')
-	high_link('TSParameter', 'Argument')
-	high_link('TSParameterReference', 'Argument')
-	high_link('TSProperty', 'Property')
-	high_link('TSPunctDelimiter', 'Delimiter')
-	high_link('TSPunctBracket', 'Delimiter')
-	high_link('TSPunctSpecial', 'Delimiter')
-	high_link('TSRepeat', 'Repeat')
-	high_link('TSString', 'String')
-	high_link('TSStringRegex', 'StringDelimiter')
-	high_link('TSStringEscape', 'StringDelimiter')
-	high_link('TSTag', 'Tag')
-	high_link('TSTagDelimiter', 'Delimiter')
-	high_link('TSStrong', 'Bold')
-	high_link('TSURI', 'URL')
-	high_link('TSWarning', 'Warning')
-	high_link('TSDanger', 'Error')
-	high_link('TSType', 'Type')
-	high_link('TSTypeBuiltin', 'TypeBuiltin')
-	high_link('TSVariable', 'None')
-	high_link('TSVariableBuiltin', 'VariableBuiltin')
-end
+local async
 
--- }}}
+async = vim.loop.new_async(vim.schedule_wrap(function()
+	local components = {
+		general_ui,
+		search_high_ui,
+		main_syntax,
+		diff,
+		markdown,
+		barbar,
+		telescope,
+		indent_blankline,
+		nvim_tree,
+		dashboard,
+		whichkey,
+		msg_underline,
+		lspsaga,
+	}
 
--- Neovim Terminal Colors {{{
+	-- Text levels {{{
 
-if vim.g.doom_one_terminal_colors then
-	vim.g.terminal_color_0 = bg
-	vim.g.terminal_color_1 = red
-	vim.g.terminal_color_2 = green
-	vim.g.terminal_color_3 = yellow
-	vim.g.terminal_color_4 = blue
-	vim.g.terminal_color_5 = violet
-	vim.g.terminal_color_6 = cyan
-	vim.g.terminal_color_7 = fg
-	vim.g.terminal_color_8 = grey
-	vim.g.terminal_color_9 = red
-	vim.g.terminal_color_10 = green
-	vim.g.terminal_color_11 = orange
-	vim.g.terminal_color_12 = blue
-	vim.g.terminal_color_13 = violet
-	vim.g.terminal_color_14 = cyan
-	vim.g.terminal_color_15 = white
-	vim.g.terminal_color_background = bg_alt
-	vim.g.terminal_color_foreground = fg_alt
-end
+	local text_colors = {
+		Normal = fg,
+		Info = blue,
+		Success = green,
+		Warning = yellow,
+		Debug = yellow,
+		Error = red,
+		Special = violet,
+		Muted = base7,
+	}
+
+	for key, _ in pairs(text_colors) do
+		apply_highlight({
+			['Text' .. key] = {
+				fg = text_colors[key],
+			},
+		})
+		apply_highlight({
+			['Text' .. key .. 'Bold'] = {
+				fg = text_colors[key],
+				gui = 'bold',
+			},
+		})
+	end
+
+	-- }}}
+
+	for _, component in ipairs(components) do
+		if not vim.tbl_isempty(component) then
+			apply_highlight(component)
+		end
+	end
+
+	-- Highlight links {{{
+
+	-- Health {{{
+
+	high_link('healthError', 'ErrorMsg')
+	high_link('healthSuccess', 'Msg')
+	high_link('healthWarning', 'WarningMsg')
+
+	-- }}}
+
+	-- Main syntax {{{
+
+	high_link('CommentURL', 'URL')
+	high_link('CommentLabel', 'CommentBold')
+	high_link('CommentSection', 'CommentBold')
+	high_link('Noise', 'Comment')
+
+	-- }}}
+
+	-- Msg {{{
+
+	high_link('Msg', 'TextSuccess')
+	high_link('MoreMsg', 'TextInfo')
+	high_link('WarningMsg', 'TextWarning')
+	high_link('Error', 'TextError')
+	high_link('ErrorMsg', 'TextError')
+	high_link('ModeMsg', 'TextSpecial')
+	high_link('Todo', 'TextWarningBold')
+
+	-- }}}
+
+	-- Gitgutter {{{
+
+	high_link('GitGutterAdd', 'DiffAddedGutter')
+	high_link('GitGutterChange', 'DiffModifiedGutter')
+	high_link('GitGutterDelete', 'DiffRemovedGutter')
+	high_link('GitGutterChangeDelete', 'DiffModifiedGutter')
+
+	high_link('GitGutterAddLineNr', 'DiffAddedGutterLineNr')
+	high_link('GitGutterChangeLineNr', 'DiffModifiedGutterLineNr')
+	high_link('GitGutterDeleteLineNr', 'DiffRemovedGutterLineNr')
+	high_link('GitGutterChangeDeleteLineNr', 'DiffModifiedGutterLineNr')
+
+	-- }}}
+
+	-- Gitsigns {{{
+
+	high_link('GitSignsAdd', 'DiffAddedGutter')
+	high_link('GitSignsChange', 'DiffModifiedGutter')
+	high_link('GitSignsDelete', 'DiffRemovedGutter')
+	high_link('GitSignsChangeDelete', 'DiffModifiedGutter')
+
+	-- }}}
+
+	-- NvimTree {{{
+
+	high_link('NvimTreeGitDirty', 'DiffModifiedGutter')
+	high_link('NvimTreeGitStaged', 'DiffModifiedGutter')
+	high_link('NvimTreeGitMerge', 'DiffModifiedGutter')
+	high_link('NvimTreeGitRenamed', 'DiffModifiedGutter')
+	high_link('NvimTreeGitNew', 'DiffAddedGutter')
+	high_link('NvimTreeGitDeleted', 'DiffRemovedGutter')
+	high_link('NvimTreeIndentMarker', 'IndentGuide')
+	high_link('NvimTreeOpenedFolderName', 'NvimTreeFolderName')
+
+	-- }}}
+
+	-- Dashboard {{{
+
+	high_link('dashboardFooter', 'dashboardHeader')
+
+	-- }}}
+
+	-- Telescope {{{
+
+	high_link('TelescopePrompt', 'TelescopeNormal')
+
+	-- }}}
+
+	-- LSP {{{
+
+	high_link('LspDiagnosticsFloatingError', 'ErrorMsg')
+	high_link('LspDiagnosticsFloatingWarning', 'Warning')
+	high_link('LspDiagnosticsFloatingInformation', 'MoreMsg')
+	high_link('LspDiagnosticsFloatingHint', 'Msg')
+	high_link('LspDiagnosticsDefaultError', 'ErrorMsg')
+	high_link('LspDiagnosticsDefaultWarning', 'WarningMsg')
+	high_link('LspDiagnosticsDefaultInformation', 'MoreMsg')
+	high_link('LspDiagnosticsDefaultHint', 'Msg')
+	high_link('LspDiagnosticsVirtualTextError', 'ErrorMsg')
+	high_link('LspDiagnosticsVirtualTextWarning', 'WarningMsg')
+	high_link('LspDiagnosticsVirtualTextInformation', 'MoreMsg')
+	high_link('LspDiagnosticsVirtualTextHint', 'Msg')
+	high_link('LspDiagnosticsUnderlineError', 'ErrorMsgUnderline')
+	high_link('LspDiagnosticsUnderlineWarning', 'WarningMsgUnderline')
+	high_link('LspDiagnosticsUnderlineInformation', 'MoreMsgUnderline')
+	high_link('LspDiagnosticsUnderlineHint', 'MsgUnderline')
+	high_link('LspDiagnosticsSignError', 'ErrorMsg')
+	high_link('LspDiagnosticsSignWarning', 'WarningMsg')
+	high_link('LspDiagnosticsSignInformation', 'MoreMsg')
+	high_link('LspDiagnosticsSignHint', 'Msg')
+	high_link('LspReferenceText', 'Bold')
+	high_link('LspReferenceRead', 'Bold')
+	high_link('LspReferenceWrite', 'Bold')
+
+	-- }}}
+
+	-- Lspsaga {{{
+
+	high_link('LspSagaDiagnosticBorder', 'Normal')
+	high_link('LspSagaDiagnosticTruncateLine', 'Normal')
+	high_link('LspFloatWinBorder', 'Normal')
+	high_link('LspSagaBorderTitle', 'Title')
+	high_link('TargetWord', 'Error')
+	high_link('ReferencesCount', 'Title')
+	high_link('ReferencesIcon', 'Special')
+	high_link('DefinitionCount', 'Title')
+	high_link('TargetFileName', 'Comment')
+	high_link('DefinitionIcon', 'Special')
+	high_link('ProviderTruncateLine', 'Normal')
+	high_link('LspSagaFinderSelection', 'Search')
+	high_link('DiagnosticTruncateLine', 'Normal')
+	high_link('DiagnosticError', 'LspDiagnosticsDefaultError')
+	high_link('DiagnosticWarning', 'LspDiagnosticsDefaultWarning')
+	high_link('DiagnosticInformation', 'LspDiagnosticsDefaultInformation')
+	high_link('DiagnosticHint', 'LspDiagnosticsDefaultHint')
+	high_link('DefinitionPreviewTitle', 'Title')
+	high_link('LspSagaShTruncateLine', 'Normal')
+	high_link('LspSagaDocTruncateLine', 'Normal')
+	high_link('LineDiagTuncateLine', 'Normal')
+	high_link('LspSagaCodeActionTitle', 'Title')
+	high_link('LspSagaCodeActionTruncateLine', 'Normal')
+	high_link('LspSagaCodeActionContent', 'Normal')
+	high_link('LspSagaRenamePromptPrefix', 'Normal')
+	high_link('LspSagaRenameBorder', 'Bold')
+	high_link('LspSagaHoverBorder', 'Bold')
+	high_link('LspSagaSignatureHelpBorder', 'Bold')
+	high_link('LspSagaCodeActionBorder', 'Bold')
+	high_link('LspSagaDefPreviewBorder', 'Bold')
+	high_link('LspLinesDiagBorder', 'Bold')
+
+	-- }}}
+
+	-- }}}
+
+	-- TreeSitter {{{
+
+	if vim.g.doom_one_enable_treesitter then
+		high_link('TSException', 'Exception')
+		high_link('TSAnnotation', 'PreProc')
+		high_link('TSAttribute', 'Attribute')
+		high_link('TSConditional', 'Conditional')
+		high_link('TSComment', 'Comment')
+		high_link('TSConstructor', 'Structure')
+		high_link('TSConstant', 'Constant')
+		high_link('TSConstBuiltin', 'Constant')
+		high_link('TSConstMacro', 'Macro')
+		high_link('TSError', 'Error')
+		high_link('TSField', 'Field')
+		high_link('TSFloat', 'Float')
+		high_link('TSFunction', 'Function')
+		high_link('TSFuncBuiltin', 'FunctionBuiltin')
+		high_link('TSFuncMacro', 'Macro')
+		high_link('TSInclude', 'Include')
+		high_link('TSKeyword', 'Keyword')
+		high_link('TSKeywordFunction', 'KeywordFunction')
+		high_link('TSLabel', 'Label')
+		high_link('TSMethod', 'Method')
+		high_link('TSNamespace', 'Directory')
+		high_link('TSNumber', 'Number')
+		high_link('TSOperator', 'Operator')
+		high_link('TSParameter', 'Argument')
+		high_link('TSParameterReference', 'Argument')
+		high_link('TSProperty', 'Property')
+		high_link('TSPunctDelimiter', 'Delimiter')
+		high_link('TSPunctBracket', 'Delimiter')
+		high_link('TSPunctSpecial', 'Delimiter')
+		high_link('TSRepeat', 'Repeat')
+		high_link('TSString', 'String')
+		high_link('TSStringRegex', 'StringDelimiter')
+		high_link('TSStringEscape', 'StringDelimiter')
+		high_link('TSTag', 'Tag')
+		high_link('TSTagDelimiter', 'Delimiter')
+		high_link('TSStrong', 'Bold')
+		high_link('TSURI', 'URL')
+		high_link('TSWarning', 'Warning')
+		high_link('TSDanger', 'Error')
+		high_link('TSType', 'Type')
+		high_link('TSTypeBuiltin', 'TypeBuiltin')
+		high_link('TSVariable', 'None')
+		high_link('TSVariableBuiltin', 'VariableBuiltin')
+	end
+
+	-- }}}
+
+	-- Neovim Terminal Colors {{{
+
+	if vim.g.doom_one_terminal_colors then
+		vim.g.terminal_color_0 = bg
+		vim.g.terminal_color_1 = red
+		vim.g.terminal_color_2 = green
+		vim.g.terminal_color_3 = yellow
+		vim.g.terminal_color_4 = blue
+		vim.g.terminal_color_5 = violet
+		vim.g.terminal_color_6 = cyan
+		vim.g.terminal_color_7 = fg
+		vim.g.terminal_color_8 = grey
+		vim.g.terminal_color_9 = red
+		vim.g.terminal_color_10 = green
+		vim.g.terminal_color_11 = orange
+		vim.g.terminal_color_12 = blue
+		vim.g.terminal_color_13 = violet
+		vim.g.terminal_color_14 = cyan
+		vim.g.terminal_color_15 = white
+		vim.g.terminal_color_background = bg_alt
+		vim.g.terminal_color_foreground = fg_alt
+	end
+	high_link('TermCursor', 'Cursor')
+
+	-- }}}
+
+	async:close()
+end))
+
+async:send()
 
 -- }}}
