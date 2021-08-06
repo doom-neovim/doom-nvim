3296c443f5313ffd4508fb591897809527bb17da
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-08-05 22:18:44 -0400

    feat(eviline): add more palettes for dynamic statusline coloring
    Added palettes:
      - nord
      - dracula
      - tokyonight

--- lua/doom/modules/config/doom-eviline.lua
+++ lua/doom/modules/config/doom-eviline.lua
@@ -1,6 +1,7 @@
 return function()
   local utils = require("doom.utils")
   local config = require("doom.core.config").load_config()
+  local get_color = require("doom.modules.config.doom-eviline-palettes").get_color
 
   local bo = vim.bo
   local gl = require("galaxyline")
@@ -19,57 +20,6 @@ return function()
   }
 
   -- {{{ Utility functions
-  -- Dynamically change statusline colors,
-  -- defaults to doom-one dark variant colors.
-  --
-  -- @tparam string name The color name to be returned
-  -- @return string
-  local function get_color(name)
-    return function()
-      local colors = {
-        bg = "#23272e",
-        fg = "#bbc2cf",
-        section_bg = "#5B6268",
-        yellow = "#ECBE7B",
-        cyan = "#46D9FF",
-        green = "#98be65",
-        orange = "#da8548",
-        magenta = "#c678dd",
-        blue = "#51afef",
-        red = "#ff6c6b",
-      }
-
-      if vim.g.colors_name == "doom-one" and vim.opt.background:get() == "light" then
-        colors = {
-          bg = "#c6c7c7",
-          fg = "#383a42",
-          fg_alt = "#9ca0a4",
-          yellow = "#986801",
-          cyan = "#0184bc",
-          green = "#50a14f",
-          orange = "#da8548",
-          magenta = "#a626a4",
-          blue = "#4078f2",
-          red = "#e45649",
-        }
-      elseif vim.g.colors_name:find("gruvbox") then
-        colors = {
-          bg = "#32302f",
-          fg = "#d4be98",
-          fg_alt = "#ddc7a1",
-          yellow = "#d8a657",
-          cyan = "#89b482",
-          green = "#a9b665",
-          orange = "#e78a4e",
-          magenta = "#d3869b",
-          blue = "#7daea3",
-          red = "#ea6962",
-        }
-      end
-      return colors[name]
-    end
-  end
-
   local function is_dashboard()
     local buftype = buffer.get_buffer_filetype()
     if buftype == "DASHBOARD" then
