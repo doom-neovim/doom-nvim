98d0018adbd8ab40653f245fed94d336e3fbb1a3
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-08-10 21:17:07 +0200

    feat(module): firenvim config module vimL

--- lua/doom/modules/config/doom-fire.lua
+++ lua/doom/modules/config/doom-fire.lua
@@ -4,5 +4,28 @@ return function()
   -- so that user can trigger nvim with keybinding instead.
   -- however, something is not working here so...
   -- NOTE: see this issue for help https://github.com/glacambre/firenvim/issues/991
-  -- vim.cmd([[ let fc['.*'] = { 'takeover': 'never' } ]])
+
+  -- required global config object
+  -- vim.cmd([[
+  --   let g:firenvim_config = {
+  --     \ 'globalSettings': {
+  --         \ 'alt': 'all',
+  --     \  },
+  --     \ 'localSettings': {
+  --         \ '.*': {
+  --             \ 'cmdline': 'neovim',
+  --             \ 'content': 'text',
+  --             \ 'priority': 0,
+  --             \ 'selector': 'textarea',
+  --             \ 'takeover': 'always',
+  --         \ },
+  --     \ }
+  --   \ }
+  -- ]])
+
+  -- -- disable nvim from triggering on entering textarea
+  -- vim.cmd([[
+  --   let fc = g:firenvim_config['localSettings']
+  --   let fc['.*'] = { 'takeover': 'never' }
+  -- ]])
 end
