827d0e61cddc890f3c88e8ff0a5bc21199a9d63a
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-09-16 22:21:39 +0430

    [Refactor]: use nvim-cmp instead of compe (#119)
    also lazyload everything and fix lsp_signature

--- lua/doom/modules/config/doom-luasnip.lua
+++ lua/doom/modules/config/doom-luasnip.lua
@@ -1,3 +1,27 @@
 return function()
-  require("luasnip.loaders.from_vscode").lazy_load()
+  local util = require("doom.utils")
+  local luasnip = require("luasnip")
+
+  luasnip.config.set_config({
+    history = true,
+    -- Update more often, :h events for more info.
+    updateevents = "TextChanged,TextChangedI",
+  })
+
+  require("luasnip/loaders/from_vscode").load()
+
+  --- <tab> to jump to next snippet's placeholder
+  local function on_tab()
+    return luasnip.jump(1) and "" or util.t("<Tab>")
+  end
+
+  --- <s-tab> to jump to next snippet's placeholder
+  local function on_s_tab()
+    return luasnip.jump(-1) and "" or util.t("<S-Tab>")
+  end
+
+  util.imap("<Tab>", on_tab, { expr = true })
+  util.smap("<Tab>", on_tab, { expr = true })
+  util.imap("<S-Tab>", on_s_tab, { expr = true })
+  util.smap("<S-Tab>", on_s_tab, { expr = true })
 end
