19efc14723e51c3ef6d26a7d8930833b7f2098a9
connorgmeean <a@cgm.codes>
2021-10-02 14:36:33 +1000

    Added a lookup table for DAP clients

--- lua/doom/modules/config/doom-dap-install.lua
+++ lua/doom/modules/config/doom-dap-install.lua
@@ -1,4 +1,6 @@
 return function()
+  local log = require("doom.extras.logging")
+  local utils = require("doom.utils")
   -- Init dap-install
   local dap_install = require("dap-install")
   dap_install.setup({
@@ -6,33 +8,57 @@ return function()
 	  installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
   })
 
-  local log = require("doom.extras.logging")
-  local utils = require("doom.utils")
-  local installed_clients = require("dap-install.api.debuggers").get_installed_debuggers()
-  -- NOTE: not all the clients follows the 'language_dbg' standard and this
-  --       can give some problems to us (maybe?)
-  local available_clients = vim.tbl_keys(require("dap-install.api.debuggers").get_debuggers())
+  local dap_lang_lookup = {
+    cpp = {'ccppr_vsc'},
+    c = {'ccppr_vsc'},
+    rust = {'ccppr_vsc'},
+    go = {'go_delve'},
+    javascript = { 'chrome', 'jsnode' },
+    typescript = { 'chrome', 'jsnode' },
+    ruby = {'ruby_vsc'}
+  }
 
-  local modules = require("doom.core.config.modules").modules
-  local langs = modules.langs
+  -- Iterates through langs and installs clients where possible
+  local install_dap_clients = function()
+    local installed_clients = require("dap-install.api.debuggers").get_installed_debuggers()
+    -- NOTE: not all the clients follows the 'language_dbg' standard and this
+    --       can give some problems to us (maybe?)
+    local available_clients = vim.tbl_keys(require("dap-install.api.debuggers").get_debuggers())
 
-  for _, lang in ipairs(langs) do
-    local lang_str = lang
-    lang = lang:gsub("%s+%+lsp", ""):gsub("%s+%+debug", "")
+    local modules = require("doom.core.config.modules").modules
+    local langs = modules.langs
 
-    -- If the +debug flag exists and the language client is not installed yet
-    if lang_str:find("%+debug") and (not utils.has_value(installed_clients, lang)) then
-      -- Try to install the client only if there is a client available for
-      -- the language, oterwise raise a warning
-      if utils.has_value(available_clients, lang) then
-        require('dap-install.core.install').install_debugger(lang)
+    for _, lang in ipairs(langs) do
+      local lang_str = lang
+      lang = lang:gsub("%s+%+lsp", ""):gsub("%s+%+debug", "")
+
+      -- DAPInstall.nvim has different names for the DAPs so sometimes we need to lookup the correct DAP to install
+      if utils.has_value(dap_lang_lookup, lang) then
+        lang = dap_lang_lookup[lang]
       else
-        log.warn(
-          "The language "
-            .. lang
-            .. ' does not have a DAP client, please remove the "+debug" flag'
-        )
+        lang = { lang }
+      end
+
+      -- Iterate over DAPs installing them one by one
+      for _, dap_name in ipairs(lang) do
+        -- If the +debug flag exists and the language client is not installed yet
+        if lang_str:find("%+debug") and (not utils.has_value(installed_clients, dap_name)) then
+
+          -- Try to install the client only if there is a client available for
+          -- the language, oterwise raise a warning
+          if utils.has_value(available_clients, dap_name) then
+            require('dap-install.core.install').install_debugger(dap_name)
+          else
+            log.warn(
+              "The language "
+                .. dap_name
+                .. ' does not have a DAP client, please remove the "+debug" flag'
+            )
+          end
+        end
       end
     end
   end
+
+  install_dap_clients()
 end
