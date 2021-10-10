fafec4d5da021f43dec0e13de0dd19cc46dd1af1
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-10-09 21:58:03 -0400

    feat: disable foldcolumn in norg docs

--- lua/doom/core/functions/init.lua
+++ lua/doom/core/functions/init.lua
@@ -161,6 +161,7 @@ M.open_docs = function()
   vim.opt_local.modified = false
   vim.opt_local.modifiable = false
   vim.opt_local.signcolumn = "no"
+  vim.opt_local.foldcolumn = "0"
   vim.opt_local.number = false
   vim.opt_local.relativenumber = false
   vim.opt_local.colorcolumn = "0"
