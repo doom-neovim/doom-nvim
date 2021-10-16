8f2f16da682547f1e7bb0aa4e023757384a62940
mordechai <mordechai.hadad01@gmail.com>
2021-10-16 22:20:56 +0300

    feat(lspmanager): Changed lspinstall to lspmanager

--- lua/doom/modules/config/doom-lspmanager.lua
+++ lua/doom/modules/config/doom-lspmanager.lua
@@ -131,13 +131,13 @@ return function()
   -- https://github.com/kabouzeid/nvim-lspinstall#advanced-configuration-recommended
   local function setup_servers()
     -- Provide the missing :LspInstall
-    require("lspinstall").setup()
+    require("lspmanager").setup()
 
     local servers = require("lspinstall").installed_servers()
     for _, server in pairs(servers) do
       -- Configure sumneko for neovim lua development
-      if server == "lua" then
-        nvim_lsp.lua.setup(lua_lsp)
+      if server == "sumneko_lua" then
+        nvim_lsp.sumneko_lua.setup(lua_lsp)
       else
         -- Use default settings for all the other language servers
         nvim_lsp[server].setup({
@@ -149,10 +149,4 @@ return function()
   end
 
   setup_servers()
-
-  -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-  require("lspinstall").post_install_hook = function()
-    setup_servers() -- reload installed servers
-    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-  end
 end
