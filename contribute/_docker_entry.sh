ba323256a443056eb5fbb5ab84f36ef74a44fc6f
connorgmeean <a@cgm.codes>
2021-10-29 17:22:49 +1100

    Fixup bashrc changes

--- contribute/_docker_entry.sh
+++ contribute/_docker_entry.sh
@@ -4,8 +4,8 @@
 # this needs to be debounced otherwise it's quite hard to exit.
 
 echo "alias doomwatch=\"chokidar '/home/doom/.config/nvim/**/*.lua' -t 100 -c 'debounce 1 | nvim'\"" >> ~/.bashrc
-echo "echo 'Use '\$ doomwatch' command to automatically restart nvim on changes to doom-nvim-contrib'." >> ~/.bashrc
-echo "echo '    This command is still buggy and hard to exit, you may have to close your terminal if you use it.  :( " >> ~/.bashrc
+echo "echo 'Use '\$ doomwatch' command to automatically restart nvim on changes to doom-nvim-contrib.'" >> ~/.bashrc
+echo "echo '    This command is still buggy and hard to exit, you may have to close your terminal if you use it.  :( '" >> ~/.bashrc
 
 echo "nvim" >> ~/.bashrc
 # Cd into workspace folder
