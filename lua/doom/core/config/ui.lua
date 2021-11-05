d72632f29b84e47ee8a4ed6f773206dfb339d8a7
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-11-05 15:20:49 -0400

    fix: doom-one being applied when other colorscheme is configured

--- lua/doom/core/config/ui.lua
+++ lua/doom/core/config/ui.lua
@@ -39,30 +39,32 @@ end
 vim.cmd("highlight WhichKeyFloat guibg=" .. config.doom.whichkey_bg)
 
 -- Set doom-one colorscheme settings
-require("colors.doom-one").setup({
-  cursor_coloring = config.doom.doom_one.cursor_coloring,
-  terminal_colors = config.doom.doom_one.terminal_colors,
-  italic_comments = config.doom.doom_one.italic_comments,
-  enable_treesitter = config.doom.doom_one.enable_treesitter,
-  transparent_background = config.doom.doom_one.transparent_background,
-  pumblend = {
-    enable = true,
-    transparency_amount = config.doom.complete_transparency,
-  },
-  plugins_integrations = {
-    neorg = true,
-    barbar = false,
-    bufferline = true,
-    gitgutter = false,
-    gitsigns = true,
-    telescope = config.doom.doom_one.telescope_highlights,
-    neogit = true,
-    nvim_tree = true,
-    dashboard = true,
-    startify = false,
-    whichkey = true,
-    indent_blankline = true,
-    vim_illuminate = true,
-    lspsaga = false,
-  },
-})
+if config.doom.colorscheme == "doom-one" then
+  require("colors.doom-one").setup({
+    cursor_coloring = config.doom.doom_one.cursor_coloring,
+    terminal_colors = config.doom.doom_one.terminal_colors,
+    italic_comments = config.doom.doom_one.italic_comments,
+    enable_treesitter = config.doom.doom_one.enable_treesitter,
+    transparent_background = config.doom.doom_one.transparent_background,
+    pumblend = {
+      enable = true,
+      transparency_amount = config.doom.complete_transparency,
+    },
+    plugins_integrations = {
+      neorg = true,
+      barbar = false,
+      bufferline = true,
+      gitgutter = false,
+      gitsigns = true,
+      telescope = config.doom.doom_one.telescope_highlights,
+      neogit = true,
+      nvim_tree = true,
+      dashboard = true,
+      startify = false,
+      whichkey = true,
+      indent_blankline = true,
+      vim_illuminate = true,
+      lspsaga = false,
+    },
+  })
+end
