8bd10ca51497ba660c535385212e992561e9800c
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-11-09 15:06:25 -0500

    feat(mappings): user defind esc sequences

--- lua/doom/core/config/init.lua
+++ lua/doom/core/config/init.lua
@@ -235,6 +235,10 @@ config.config = {
       completion = true,
     },
 
+	-- sequences used for escaping insert mode
+    -- @default = { 'jk', 'kj' }
+    escape_sequences = { 'jk', 'kj' },
+
     -- Disable or enable Doom autocommands, this can break some configuration options (they will stop working)
     -- e.g. preserve_edit_pos or autosave
     --
