04bbd07e17d75ba9f0eaeff586c85b8bb1c00a5a
connorgmeean <a@cgm.codes>
2021-11-18 21:37:15 +1100

    Fixed :DoomReport not collecting logs

--- lua/doom/core/functions/init.lua
+++ lua/doom/core/functions/init.lua
@@ -184,11 +184,13 @@ functions.create_report = function()
     local today_logs = {}
     local doom_logs = vim.split(fs.read_file(system.doom_logs), "\n")
     for _, doom_log in ipairs(doom_logs) do
-      if
-        string.find(doom_log, "ERROR " .. log_date_format)
-        or string.find(doom_log, "WARN  " .. log_date_format)
-      then
-        table.insert(today_logs, doom_log)
+      local preinfo = doom_log:match('%[(.+)%]')
+      if preinfo ~= nil then
+        local is_current_day = preinfo:find(os.date('%a %d %b')) ~= nil and preinfo:find(os.date('%Y')) ~= nil
+        local error_or_warn = preinfo:find('ERROR ') or preinfo:find('WARN ')
+        if error_or_warn and is_current_day then
+          table.insert(today_logs, doom_log)
+        end
       end
     end
 
