2168af3a22125febe647291fb5b3f0dc1a107980
connorgmeean <a@cgm.codes>
2022-03-03 10:21:31 +1100

    feat(langs): Add haskell language support

---
+++ lua/doom/modules/langs/java/init.lua
@@ -0,0 +1,33 @@
+local java = {}
+
+java.settings = {
+  language_server_name = 'jdtls',
+}
+
+java.autocommands = {
+  {
+    "FileType",
+    "sh",
+    function()
+      local langs_utils = require('doom.modules.langs.utils')
+      langs_utils.use_lsp(doom.modules.java.settings.language_server_name)
+      
+      vim.defer_fn(function()
+        require("nvim-treesitter.install").ensure_installed("java")
+      end, 0)
+
+      -- Setup null-ls
+      if doom.linter then
+        local null_ls = require("null-ls")
+
+        langs_utils.use_null_ls_source({
+          null_ls.builtins.formatting.shfmt
+        })
+      end
+
+    end,
+    once = true,
+  },
+}
+
+return java
