23d28cc5f4d606a9fe7595a22de839fa64661510
connorgmeean <a@cgm.codes>
2022-03-03 12:45:14 +1100

    feat(config): Added schemastore to `config` language module.

--- lua/doom/modules/langs/config/init.lua
+++ lua/doom/modules/langs/config/init.lua
@@ -3,21 +3,48 @@ local config = {}
 -- TODO: Investigate broken yaml/toml language servers
 
 config.settings = {
+  json_schemas = {
+    '.eslintrc',
+    'package.json',
+    'prettierrc.json',
+    'tsconfig.json'
+  },
   json_language_server_name = 'jsonls',
   -- toml_language_server_name = 'taplo', -- Currently broken
   -- yaml_language_server_name = 'yamlls', -- Currently broken
 }
+
+config.packages = {
+  ["SchemaStore.nvim"] = {
+    "b0o/SchemaStore.nvim",
+    commit = "df5e98d3b3c93e9857908fce8a219360f81c5e32",
+    ft = { "json", "yaml", "toml" }
+  },
+}
+config.configure_functions = {}
+config.configure_functions["SchemaStore.nvim"] = function()
+  local langs_utils = require('doom.modules.langs.utils')
+  langs_utils.use_lsp(doom.modules.config.settings.json_language_server_name, {
+    config = {
+      settings = {
+        json = {
+          schemas = require('schemastore').json.schemas {
+            select = doom.modules.config.settings.json_schemas,
+          },
+        },
+      },
+    }
+  })
+end
 
 config.autocommands = {
   {
     "FileType",
     "json,yaml,toml",
     function()
-      local langs_utils = require('doom.modules.langs.utils')
-      langs_utils.use_lsp(doom.modules.config.settings.json_language_server_name)
       -- langs_utils.use_lsp(doom.modules.config.settings.toml_language_server_name)
       -- langs_utils.use_lsp(doom.modules.config.settings.yaml_language_server_name)
-      
+
       vim.defer_fn(function()
         require("nvim-treesitter.install").ensure_installed("json5", "yaml", "toml")
       end, 0)
