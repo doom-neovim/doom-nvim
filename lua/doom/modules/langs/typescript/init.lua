ba21f46258bbe54d43fa16012b7f9b6ee1aac8e7
connorgmeean <a@cgm.codes>
2022-03-16 08:46:05 +1100

    fix(typescript): Add `tsx` file support

--- lua/doom/modules/langs/typescript/init.lua
+++ lua/doom/modules/langs/typescript/init.lua
@@ -6,20 +6,20 @@ typescript.settings = {
 typescript.autocommands = {
   {
     "FileType",
-    "typescript",
+    "typescript,typescriptreact",
     function()
       local langs_utils = require('doom.modules.langs.utils')
       langs_utils.use_lsp('tsserver')
-      
+
       vim.defer_fn(function()
         local ts_install = require("nvim-treesitter.install")
         ts_install.ensure_installed("typescript")
       end, 0)
-      
+
       -- Setup null-ls
       if doom.modules.linter then
         local null_ls = require("null-ls")
-      
+
         langs_utils.use_null_ls_source({
           null_ls.builtins.formatting.eslint_d,
           null_ls.builtins.code_actions.eslint_d,
