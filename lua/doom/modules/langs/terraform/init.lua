4b9053413f6bfbe989f9dcd9711b3d7e4ab2c49b
connorgmeean <a@cgm.codes>
2022-03-16 08:53:34 +1100

    feat(langs): Added terraform language support

---
+++ lua/doom/modules/langs/terraform/init.lua
@@ -0,0 +1,21 @@
+local terraform = {}
+
+terraform.settings = {
+}
+
+terraform.autocommands = {
+  {
+    "FileType",
+    "terraform",
+    function()
+      local langs_utils = require('doom.modules.langs.utils')
+      langs_utils.use_lsp('terraformls')
+
+      local ts_install = require("nvim-treesitter.install")
+      ts_install.ensure_installed("hcl")
+    end,
+    once = true,
+  },
+}
+
+return terraform
