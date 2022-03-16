122223a4794c05fca3aa016ffbbbd92b5e435fdc
connorgmeean <a@cgm.codes>
2022-03-17 09:28:44 +1100

    feat: Documented and re-formatted modules.lua

--- modules.lua
+++ modules.lua
@@ -7,65 +7,77 @@
 
 return {
   features = {
-    "auto_session",
-    "autopairs",
-    "colorizer",
-    -- "dap",
-    "dashboard",
-    -- "doom_themes",
-    "editorconfig",
-    -- "explorer",
-    -- "firenvim",
-    -- "formatter",
-    "gitsigns",
-    "illuminate",
-    "indentlines",
-    "comment",
-    -- "lazygit",
-    "linter",
-    "lsp",
-    -- "minimap",
-    -- "neogit",
-    -- "neorg",
-    "range_highlight",
-    -- "ranger",
-    -- "restclient",
-    -- "show_registers",
-    "snippets",
-    "statusline",
-    "fidget",
-    "projects",
-    -- "suda",
-    -- "superman",
-    -- "symbols",
-    "auto_install",
-    "annotations",
-    "tabline",
-    "telescope",
-    -- "terminal",
-    "todo_comments",
-    "trouble",
-    "whichkey",
-    -- "zen",
+    -- Language features
+    "annotations",        -- Code annotation generator
+    "auto_install",       -- Auto install LSP providers
+    "autopairs",          -- Automatically close character pairs
+    "comment",            -- Adds keybinds to comment in any language
+    "linter",             -- Linting and formatting for languages
+    "lsp",                -- Code completion
+    "snippets",           -- Code snippets for all languages
+
+    -- Editor
+    "auto_session",       -- Remember sessions between loads
+    "colorizer",          -- Show colours in neovim
+    "editorconfig",       -- Support editorconfig files
+    "gitsigns",           -- Show git changes in sidebar
+    "illuminate",         -- Highlight other copies of the word you're hovering on
+    "indentlines",        -- Show indent lines with special characters
+    "range_highlight",    -- Highlight selected range from commands
+    "todo_comments",      -- Highlight TODO: comments
+
+    -- UI
+    "fidget",             -- Check status of LSP loading
+    "tabline",            -- Tab bar buffer switcher
+    "dashboard",          -- A pretty dashboard upon opening
+    "trouble",            -- A pretty diagnostic viewer
+    "statusline",         -- A pretty status line at the bottom of the buffer
+    -- "minimap",         -- Shows current position in document
+
+    -- Tools
+    -- "dap",             -- Debug code through neovim
+    "explorer",           -- An enhanced filetree explorer
+    -- "firenvim",        -- Embed neovim in your browser
+    "telescope",          -- Fuzzy searcher to find files, grep code and more
+    "neorg",              -- Organise your life
+    "whichkey",           -- An interactive sheet
+    "projects",           -- Quickly switch between projects
+
+    -- "doom_themes",     -- Extra themes for doom
+    -- "lazygit",         -- Lazy git integration
+    -- "neogit",          -- A git client for neovim
+    -- "ranger",          -- File explorer in neovim (TODO: Test)
+    -- "restclient",      -- Test HTTP requests from neovim (TODO: Test)
+    -- "show_registers",  -- Show and navigate between registers
+    -- "suda",            -- Save using sudo when necessary
+    -- "superman",        -- Read unix man pages in neovim
+    -- "symbols",         -- Navigate between code symbols using telescope
+    -- "terminal",        -- Integrated terminal in neovim
+    -- "zen",             -- Distractionless coding
   },
   langs = {
+    -- Scripts
     "lua",
     "python",
     "bash",
-    "config",
 
+    -- Web
     "javascript",
     "typescript",
     "css",
     "vue",
     "tailwindcss",
 
+    -- Compiled
     "rust",
     "cpp",
 
+    -- JIT
     "c_sharp",
     "kotlin",
     "java",
+
+    "config",             -- JSON, YAML, TOML
   }
 }
 
