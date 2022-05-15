ab4493e3fc33eaf2d1866f60b55d670c9ed380d1
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-05-15 13:37:41 -0400

    doomui refactor and prepare pickers to work again

--- lua/user/modules/features/doom_ui/init.lua
+++ lua/user/modules/features/doom_ui/init.lua
@@ -111,9 +111,9 @@ local function ts_get_doom_captures(buf, doom_capture_name)
 
   for id, node, _ in qp:iter_captures(root, buf, root:start(), root:end_()) do
     local name = qp.captures[id]
-	if name == doom_capture_name then
-      table.insert(t_matched_captures, node)
-	end
+	  if name == doom_capture_name then
+        table.insert(t_matched_captures, node)
+	  end
    end
    return t_matched_captures
 end
@@ -137,6 +137,15 @@ conf_ui.filter_modules = function(filter)
   -- end
 
   return filtered
+end
+
+local function flatten_regular_binds_tree(nest_tree)
+
+end
+
+local function flatten_ts_nest_tree(ts_nest_table)
+  local ts_nest_flat = {}
+  return ts_nest_flat
 end
 
 --
@@ -352,139 +361,139 @@ local function shell_mod_remove_dir(rm_path)
   vim.cmd(rm)
 end
 
-
-MODULE ACTIONS
-
-
-
--- local function m_edit(c)
---   if c.selected_module ~= nil then
---     vim.cmd(string.format(":e %s%s%s", c.selected_module.path, system.sep, "init.lua"))
---   end
--- end
 --
--- local function m_rename(c)
---   nui_input("NEW NAME", function(value)
---     if not check_if_module_name_exists(c, value) then
---       print("old name: ", c.selected_module.name, ", new name:", value)
--- --       local new_name = value
--- --
--- --       local buf, _ = transform_root_mod_file(m, function(buf, node, capt, node_text)
--- --         local sr, sc, er, ec = node:range()
--- --         if capt == "modules.enabled" then
--- --           local offset = 1
--- --           local exact_match = string.match(node_text, m.name)
--- --           if exact_match then
--- --             vim.api.nvim_buf_set_text(
--- --               buf,
--- --               sr,
--- --               sc + offset,
--- --               sr,
--- --               sc + offset + string.len(m.name),
--- --               { value }
--- --             )
--- --           end
--- --         elseif capt == "modules.disabled" then
--- --           local offset = 4
--- --           local exact_match = string.match(node_text, m.name)
--- --           if exact_match then
--- --             vim.api.nvim_buf_set_text(
--- --               buf,
--- --               sr,
--- --               sc + offset,
--- --               sr,
--- --               sc + offset + string.len(m.name),
--- --               { value }
--- --             )
--- --           end
--- --         end
--- --       end)
--- --
--- --       write_to_root_modules_file(buf)
--- --       shell_mod_rename_dir(m.section, m.path, new_name)
---     end
---   end)
--- end
+-- MODULE ACTIONS
 --
--- local function m_create(c)
---   local new_name
---   local for_section
+
+
+local function m_edit(c)
+  if c.selected_module ~= nil then
+    vim.cmd(string.format(":e %s%s%s", c.selected_module.path, system.sep, "init.lua"))
+  end
+end
+
+local function m_rename(c)
+  nui_input("NEW NAME", function(value)
+    if not check_if_module_name_exists(c, value) then
+      print("old name: ", c.selected_module.name, ", new name:", value)
+--       local new_name = value
 --
---   nui_menu("CONFIRM CREATE", conf_ui.settings.confirm_alternatives, function(value)
---     if value.text == "yes" then
---       new_name = c.new_module_name
---       nui_menu("FOR SECTION:", conf_ui.settings.section_alternatives, function(value)
---         for_section = value.text
---         print("create mod >> new name:", for_section .. " > " .. new_name)
--- --         if not check_if_module_name_exists(c, { section = nil }, value) then
--- --           local buf, smll = transform_root_mod_file({ section = value.text })
--- --           -- print("smll: ", smll)
--- --           new_name = vim.trim(new_name, " ")
--- --           vim.api.nvim_buf_set_lines(buf, smll, smll, true, { '"' .. new_name .. '",' })
--- --           write_to_root_modules_file(buf)
--- --           shell_mod_new(for_section, new_name)
---         -- end
+--       local buf, _ = transform_root_mod_file(m, function(buf, node, capt, node_text)
+--         local sr, sc, er, ec = node:range()
+--         if capt == "modules.enabled" then
+--           local offset = 1
+--           local exact_match = string.match(node_text, m.name)
+--           if exact_match then
+--             vim.api.nvim_buf_set_text(
+--               buf,
+--               sr,
+--               sc + offset,
+--               sr,
+--               sc + offset + string.len(m.name),
+--               { value }
+--             )
+--           end
+--         elseif capt == "modules.disabled" then
+--           local offset = 4
+--           local exact_match = string.match(node_text, m.name)
+--           if exact_match then
+--             vim.api.nvim_buf_set_text(
+--               buf,
+--               sr,
+--               sc + offset,
+--               sr,
+--               sc + offset + string.len(m.name),
+--               { value }
+--             )
+--           end
+--         end
 --       end)
+--
+--       write_to_root_modules_file(buf)
+--       shell_mod_rename_dir(m.section, m.path, new_name)
+    end
+  end)
+end
+
+local function m_create(c)
+  local new_name
+  local for_section
+
+  nui_menu("CONFIRM CREATE", conf_ui.settings.confirm_alternatives, function(value)
+    if value.text == "yes" then
+      new_name = c.new_module_name
+      nui_menu("FOR SECTION:", conf_ui.settings.section_alternatives, function(value)
+        for_section = value.text
+        print("create mod >> new name:", for_section .. " > " .. new_name)
+--         if not check_if_module_name_exists(c, { section = nil }, value) then
+--           local buf, smll = transform_root_mod_file({ section = value.text })
+--           -- print("smll: ", smll)
+--           new_name = vim.trim(new_name, " ")
+--           vim.api.nvim_buf_set_lines(buf, smll, smll, true, { '"' .. new_name .. '",' })
+--           write_to_root_modules_file(buf)
+--           shell_mod_new(for_section, new_name)
+        -- end
+      end)
+    end
+  end)
+end
+
+-- TODO: what happens if you try to remove a module that has been disabled ?? account for disabled in modules.lua
+--
+local function m_delete(c)
+  nui_menu("CONFIRM DELETE", conf_ui.settings.confirm_alternatives, function(value)
+    if value.text == "yes" then
+	print("delete module: ", c.selected_module.section .. " > " .. c.selected_module.name)
+--       local buf, _ = transform_root_mod_file(m, function(buf, node, capt, node_text)
+--         local sr, sc, er, ec = node:range()
+--         if capt == "modules.enabled" then
+--           -- local offset = 1
+--           local exact_match = string.match(node_text, m.name)
+--           if exact_match then
+--             vim.api.nvim_buf_set_text(buf, sr, sc, er, ec + 1, { "" })
+--           end
+--         elseif capt == "modules.disabled" then
+--           -- local offset = 4
+--           local exact_match = string.match(node_text, m.name)
+--           if exact_match then
+--             vim.api.nvim_buf_set_text(buf, sr, sc, er, ec + 1, { "" })
+--           end
+--         end
+--       end)
+--
+--       write_to_root_modules_file(buf)
+--       shell_mod_remove_dir(m.path) -- shell: rm m.path
+    end
+  end)
+end
+
+local function m_toggle(c)
+  print("toggle: ", c.selected_module.name)
+--   local buf, _ = transform_root_mod_file(m, function(buf, node, capt, node_text)
+--     local sr, sc, er, ec = node:range()
+--     if string.match(node_text, m.name) then
+--       if capt == "modules.enabled" then
+--         vim.api.nvim_buf_set_text(buf, sr, sc, er, ec, { "-- " .. node_text })
+--       elseif capt == "modules.disabled" then
+--         vim.api.nvim_buf_set_text(buf, sr, sc, er, ec, { node_text:sub(4) })
+--       end
 --     end
 --   end)
--- end
---
--- -- TODO: what happens if you try to remove a module that has been disabled ?? account for disabled in modules.lua
--- --
--- local function m_delete(c)
---   nui_menu("CONFIRM DELETE", conf_ui.settings.confirm_alternatives, function(value)
---     if value.text == "yes" then
--- 	print("delete module: ", c.selected_module.section .. " > " .. c.selected_module.name)
--- --       local buf, _ = transform_root_mod_file(m, function(buf, node, capt, node_text)
--- --         local sr, sc, er, ec = node:range()
--- --         if capt == "modules.enabled" then
--- --           -- local offset = 1
--- --           local exact_match = string.match(node_text, m.name)
--- --           if exact_match then
--- --             vim.api.nvim_buf_set_text(buf, sr, sc, er, ec + 1, { "" })
--- --           end
--- --         elseif capt == "modules.disabled" then
--- --           -- local offset = 4
--- --           local exact_match = string.match(node_text, m.name)
--- --           if exact_match then
--- --             vim.api.nvim_buf_set_text(buf, sr, sc, er, ec + 1, { "" })
--- --           end
--- --         end
--- --       end)
--- --
--- --       write_to_root_modules_file(buf)
--- --       shell_mod_remove_dir(m.path) -- shell: rm m.path
---     end
---   end)
--- end
---
--- local function m_toggle(c)
---   print("toggle: ", c.selected_module.name)
--- --   local buf, _ = transform_root_mod_file(m, function(buf, node, capt, node_text)
--- --     local sr, sc, er, ec = node:range()
--- --     if string.match(node_text, m.name) then
--- --       if capt == "modules.enabled" then
--- --         vim.api.nvim_buf_set_text(buf, sr, sc, er, ec, { "-- " .. node_text })
--- --       elseif capt == "modules.disabled" then
--- --         vim.api.nvim_buf_set_text(buf, sr, sc, er, ec, { node_text:sub(4) })
--- --       end
--- --     end
--- --   end)
--- --   write_to_root_modules_file(buf)
--- end
---
--- local function m_move(buf, config)
--- --   -- move module into into (features/langs)
--- --   -- 1. nui menu select ( other sections than self)
--- --   -- 2. move module dir
--- --   -- 3. transform `modules.lua`
--- end
---
--- local function m_merge(buf, config)
--- --   -- 1. new prompt for B
--- --   -- 2. select which module to pull into A
--- --   -- 3. do...
--- end
+--   write_to_root_modules_file(buf)
+end
+
+local function m_move(buf, config)
+--   -- move module into into (features/langs)
+--   -- 1. nui menu select ( other sections than self)
+--   -- 2. move module dir
+--   -- 3. transform `modules.lua`
+end
+
+local function m_merge(buf, config)
+--   -- 1. new prompt for B
+--   -- 2. select which module to pull into A
+--   -- 3. do...
+end
 
 -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
@@ -543,9 +552,9 @@ conf_ui.parse_nest_tables_meta_data = function(buf, node, accumulated,level)
         return accumulated
       else
         accumulated["prefix"] = the_node
-	accumulated["doom_category"] = "binds_leaf"
+	      accumulated["doom_category"] = "binds_leaf"
         local nt = tsq.get_node_text(the_node, buf)
-	accumulated["prefix_text"] = nt
+	      accumulated["prefix_text"] = nt
         special_cnt = special_cnt + 1
       end
     end
@@ -630,14 +639,6 @@ end
 -- local function tableify_data_for_results()
 -- end
 
-local function flatten_regular_binds_tree(nest_tree)
-
-end
-
-local function flatten_ts_nest_tree(ts_nest_table)
-  local ts_nest_flat = {}
-  return ts_nest_flat
-end
 
 -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
@@ -700,14 +701,6 @@ conf_ui.doom_modules_picker = function(c)
 	      end}
 	}
 
-  -- local tlegend = " [<cr>:edit,<c-(e:create,r:rename,t:toggle,u:remove)]"
-local tlegend = "["
-      for _,m in pairs(t_mappings) do
-	tlegend = tlegend .. m[1] ..","
-      end
-	tlegend = tlegend.. "]"
-
-
   local function create_maps()
   end
 
@@ -717,18 +710,15 @@ local tlegend = "["
       buf_ref = nil,
       history = {},
       opts = {
-	title = "Doom Modules"
+	    title = "Doom Modules"
       }
     }
   end
 
-  -- print(vim.inspect(c.data))
-
-  -- local opts = opts or require("telescope.themes").get_cursor()
   local doom_modules_theme = require("telescope.themes").get_dropdown()
 
   require("telescope.pickers").new(doom_modules_theme, {
-    prompt_title = "Doom Modules Manager" .. tlegend,
+    prompt_title = "Doom Modules Manager",
     finder = require("telescope.finders").new_table({
       results = c.data,
       entry_maker = display_all_modules_list,
@@ -745,10 +735,6 @@ end
 
 --
 -- PICKER -> DOOM MAIN MENU
---
-
--- TODO: make the most useful base picker where you can get an overview
--- of all available categories that you can enter and browse.
 --
 
 conf_ui.doom_main_menu_picker = function(c)
@@ -815,6 +801,12 @@ end
 -- 	so that any table can be recursively pickyfied.
 conf_ui.doom_settings_picker = function(c)
 
+  local function ts_table_picker_prepare()
+  end
+
+  local function ts_table_picker_mappings()
+  end
+
   if c == nil then c = {} end
   if c.settings_table == nil then
 	c["settings_table"] = {}
@@ -864,6 +856,8 @@ conf_ui.doom_settings_picker = function(c)
     }),
     sorter = require("telescope.config").values.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr, map)
+
+      -- REFACTOR: move this to mappings func above
         actions_set.select:replace(function()
           local fuzzy = selection(prompt_bufnr)
 	  -- print(vim.inspect(fuzzy.value:type()))
