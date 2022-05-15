a62e345dd08dda97a3d023c733612da6511b1ed5
connorgmeean <a@cgm.codes>
2022-05-15 12:33:14 +1000

    fix(modules.todo_comments): Fixed plugin not loading.

--- lua/doom/modules/features/todo_comments/init.lua
+++ lua/doom/modules/features/todo_comments/init.lua
@@ -6,7 +6,6 @@ todo_comments.packages = {
   ["todo-comments.nvim"] = {
     "folke/todo-comments.nvim",
     commit = "98b1ebf198836bdc226c0562b9f906584e6c400e",
-    event = "ColorScheme",
   },
 }
 
