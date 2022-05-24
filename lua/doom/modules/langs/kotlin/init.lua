00ad9631e6850db0db38dd67345f60bced390747
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-05-24 23:44:51 +0800

    feat(langs): setup kotlin formatter

--- lua/doom/modules/langs/kotlin/init.lua
+++ lua/doom/modules/langs/kotlin/init.lua
@@ -13,6 +13,14 @@ kotlin.autocmds = {
       langs_utils.use_lsp(doom.langs.kotlin.settings.language_server_name)
 
       require("nvim-treesitter.install").ensure_installed("kotlin")
+      -- Setup null-ls
+      if doom.modules.linter then
+        local null_ls = require("null-ls")
+
+        langs_utils.use_null_ls_source({
+          null_ls.builtins.formatting.ktlint
+        })
+      end
     end,
     once = true,
   },
