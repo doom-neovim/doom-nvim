05b090d5dafa2af419e6a4229179c7132d9c6f11
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-06-10 05:52:23 -0400

    color utils

--- lua/doom/modules/features/colors/init.lua
+++ lua/doom/modules/features/colors/init.lua
@@ -2,6 +2,8 @@ local colorpicker = {}
 
 -- TODO: rename into something more useful
 
+
+
 -- norcalli/nvim-colorizer.lua - A high-performance color highlighter for Neovim which has no external dependencies!.
 -- sunjon/Shade.nvim - Shade is a Neovim plugin that dims your inactive windows, making it easier to see the active window at a glance.
 -- winston0410/range-highlight.nvim - An extremely lightweight plugin (~ 120loc) that highlights ranges you have entered in commandline.
@@ -11,6 +13,7 @@ local colorpicker = {}
 colorpicker.settings = {}
 
 colorpicker.packages = {
+  -- https://github.com/max397574/colortils.nvim -- <<<<<< this one!!!
   ["vCoolor.vim"] = { "KabbAmine/vCoolor.vim" }, -- open color picker / requires mouse to select color
   -- https://github.com/folke/lsp-colors.nvim
 }
