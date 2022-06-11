a10b974a770f9cc27ab8d41d39790cdf9be1f625
connorgmeean <a@cgm.codes>
2022-06-11 13:35:53 +1000

    fix(colorizer): Switched to more reliable autocommand

--- lua/doom/modules/features/colorizer/init.lua
+++ lua/doom/modules/features/colorizer/init.lua
@@ -10,7 +10,7 @@ colorizer.packages = {
   ["nvim-colorizer.lua"] = {
     "norcalli/nvim-colorizer.lua",
     commit = "36c610a9717cc9ec426a07c8e6bf3b3abcb139d6",
-    event = "ColorScheme",
+    event = "WinEnter",
   },
 }
 
