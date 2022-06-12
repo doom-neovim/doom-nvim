5fb652d42f5ad491812b8e84f1a86853cebbc90a
connorgmeean <a@cgm.codes>
2022-06-12 14:56:37 +1000

    feat(core): Improve starttime, disable builtin plugins.

--- lua/doom/core/init.lua
+++ lua/doom/core/init.lua
@@ -4,14 +4,36 @@
 --  Entrypoint for the doom-nvim framework.
 --
 --]]
---- Main Doom configuration file
---- This file loads all doom core components
---- (ui, options, doomrc, etc)
 
-local utils = require("doom.utils")
+-- Disable vim builtins for faster startup time
+local g = vim.g
+
+g.loaded_gzip = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_2html_plugin = 1
+
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
+g.loaded_netrwFileHandlers = 1
 
 -- Sets the `doom` global object
 require("doom.core.doom_global")
+
+local utils = require("doom.utils")
 
 -- Boostraps the doom-nvim framework, runs the user's `config.lua` file.
 local config = utils.safe_require("doom.core.config")
