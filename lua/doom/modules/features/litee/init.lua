4068b180ba76982c2b77e08285f80bfbc411b27a
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-06-13 10:41:03 -0400

    fix(litee): config

--- lua/doom/modules/features/litee/init.lua
+++ lua/doom/modules/features/litee/init.lua
@@ -6,13 +6,16 @@ litee.packages = {
   ["litee.nvim"] = {
     "ldelossa/litee.nvim",
     -- opt = "true",
-    config = function()
-      require("litee").setup({})
-    end,
   },
   ["litee-calltree.nvim"] = { "ldelossa/litee-calltree.nvim" },
   ["litee-bookmarks.nvim"] = { "ldelossa/litee-bookmarks.nvim" },
   ["litee-symboltree.nvim"] = { "ldelossa/litee-symboltree.nvim" },
 }
+
+litee.config = {}
+
+litee.config["litee.nvim"] = function()
+  require("litee").setup({})
+end
 
 return litee
