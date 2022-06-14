8cea60e128a63d1573c73cb40d6d4ae7f724c170
connorgmeean <a@cgm.codes>
2022-06-14 10:21:39 +1000

    Revert "feat(modules.lsp/snippets): Lazy load lsp/snippets."
    This reverts commit 565321eab195b7c1367db4939afefa409524e96e.

--- lua/doom/modules/features/lsp/init.lua
+++ lua/doom/modules/features/lsp/init.lua
@@ -92,8 +92,7 @@ lsp.packages = {
   ["nvim-cmp"] = {
     "hrsh7th/nvim-cmp",
     commit = "15c7bf7c0dfb7c75eb526c53f9574633c13dc22d",
-    event = is_module_enabled("snippets") and nil or "InsertEnter *", -- Rely on luasnips InsertEnter * event
-    after = is_module_enabled("snippets") and "luasnip" or nil,
+    after = is_module_enabled("snippets") and "LuaSnip" or nil,
   },
   ["cmp-nvim-lua"] = {
     "hrsh7th/cmp-nvim-lua",
