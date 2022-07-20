d802f587d1d9314c4f84ec570936adab5d922d71
connorgmeean <a@cgm.codes>
2022-07-20 12:48:08 +1000

    feat(langs): Added support for ruby language.

---
+++ lua/doom/modules/langs/ruby/init.lua
@@ -0,0 +1,31 @@
+local ruby = {}
+
+ruby.settings = {
+  language_server_name = "solargraph",
+}
+
+ruby.autocmds = {
+  {
+    "BufWinEnter",
+    "*.rb",
+    function()
+      local langs_utils = require("doom.modules.langs.utils")
+      langs_utils.use_lsp(doom.langs.ruby.settings.language_server_name)
+
+      require("nvim-treesitter.install").ensure_installed("ruby")
+
+      -- Setup null-ls
+      if doom.features.linter then
+        local null_ls = require("null-ls")
+
+        langs_utils.use_null_ls_source({
+          null_ls.builtins.diagnostics.rubocop,
+          null_ls.builtins.diagnostics.mypy,
+        })
+      end
+    end,
+    once = true,
+  },
+}
+
+return ruby
