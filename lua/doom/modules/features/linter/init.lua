0294e7c09af1787f887dd8065f77b3f85b1f13a8
connorgmeean <a@cgm.codes>
2022-08-01 13:38:56 +1000

    fix(linter): Increased formatting timeout.

--- lua/doom/modules/features/linter/init.lua
+++ lua/doom/modules/features/linter/init.lua
@@ -2,6 +2,9 @@ local linter = {}
 
 linter.settings = {
   format_on_save = false,
+  null_ls_settings = {
+    default_timeout = 2000,
+  }
 }
 
 linter.packages = {
@@ -16,11 +19,11 @@ linter.configs = {}
 linter.configs["null-ls.nvim"] = function()
   local null_ls = require("null-ls")
 
-  null_ls.setup({
+  local null_ls_settings = doom.features.linter.settings.null_ls_settings
+  null_ls.setup(vim.tbl_deep_extend("force", null_ls_settings, {
     on_attach = function(client)
-      if
-        client.server_capabilities.documentFormattingProvider
-        and doom.features.linter.settings.format_on_save
+      if client.server_capabilities.documentFormattingProvider
+          and doom.features.linter.settings.format_on_save
       then
         vim.cmd([[
         augroup LspFormatting
@@ -30,17 +33,20 @@ linter.configs["null-ls.nvim"] = function()
         ]])
       end
     end,
-  })
+  }))
 end
 
 linter.binds = {
   {
     "<leader>cf",
     function()
+      local null_ls_settings = doom.features.linter.settings.null_ls_settings
       if type(vim.lsp.buf.format) == "function" then
-        vim.lsp.buf.format()
+        vim.lsp.buf.format({
+          timeout_ms = null_ls_settings.default_timeout
+        })
       else
-        vim.lsp.buf.formatting_sync()
+        vim.lsp.buf.formatting_sync(nil, null_ls_settings.default_timeout)
       end
     end,
     name = "Format/Fix",
