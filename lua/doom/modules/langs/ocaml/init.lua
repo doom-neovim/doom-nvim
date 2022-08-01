50bb8d14ca250621c594d89109526f871185881d
Török Edwin <edwintorok@users.noreply.github.com>
2022-08-01 12:25:25 +0100

    feat(langs,ocaml): add language (#373)
    Uses https://ocaml.org/p/ocaml-lsp-server/ as language server that can
    be installed via `opam`.
    
    Signed-off-by: Edwin Török <edwin@etorok.net>

---
+++ lua/doom/modules/langs/ocaml/init.lua
@@ -0,0 +1,26 @@
+local ocaml = {}
+
+ocaml.settings = {
+  language_server_name = "ocamllsp",
+}
+
+ocaml.autocmds = {
+  {
+    "Filetype",
+    "ocaml,ocaml_interface,ocamllex",
+    function()
+      local langs_utils = require("doom.modules.langs.utils")
+      langs_utils.use_lsp(doom.langs.ocaml.settings.language_server_name)
+
+      vim.schedule(function()
+        require("nvim-treesitter.install").ensure_installed("ocaml","ocaml_interface")
+        if vim.fn.executable("tree-sitter-cli") == 1 then
+            require("nvim-treesitter.install").ensure_installed("ocamllex")
+        end
+      end)
+    end,
+    once = true,
+  },
+}
+
+return ocaml
