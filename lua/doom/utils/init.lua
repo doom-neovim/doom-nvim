46fdab9d83a978fc424d0693c6169f3ce546919a
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-09-18 09:25:17 -0400

    copy stuff from doom/util/init.lua

--- lua/doom/utils/init.lua
+++ lua/doom/utils/init.lua
@@ -265,4 +265,23 @@ utils.iter_string_at = function(str, sep)
   return string.gmatch(str, "([^" .. sep .. "]+)")
 end
 
+
+utils.get_buf_handle = function(path)
+  local buf
+  if path ~= nil then
+    buf = vim.uri_to_bufnr(vim.uri_from_fname(path))
+  else
+    buf = vim.api.nvim_get_current_buf()
+  end
+  return buf
+end
+
+utils.tbl_merge = function(t1, t2)
+  local ret = vim.deepcopy(t1)
+  for _, p in ipairs(t2) do
+    table.insert(ret, p)
+  end
+  return ret
+end
+
 return utils
