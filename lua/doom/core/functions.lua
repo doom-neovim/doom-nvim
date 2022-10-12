090c43e54ed31a4fe999aa93d143088ba6d1d1ad
connorgmeean <a@cgm.codes>
2022-10-12 21:02:58 +1100

    feat(core): Add `:DoomNuke mason` option to delete all mason packages.

--- lua/doom/core/functions.lua
+++ lua/doom/core/functions.lua
@@ -178,9 +178,8 @@ end
 -- Set the indent and tab related numbers.
 -- Negative numbers mean tabstop -- Really though? Tabs?
 functions.set_indent = function()
-  local indent = tonumber(
-    vim.fn.input("Set indent (>0 uses spaces, <0 uses tabs, 0 uses vim defaults): ")
-  )
+  local indent =
+    tonumber(vim.fn.input("Set indent (>0 uses spaces, <0 uses tabs, 0 uses vim defaults): "))
   if not indent then
     indent = -8
   end
@@ -257,7 +256,7 @@ end
 functions.nuke = function(target)
   if target == nil or #target == 0 then
     vim.notify(
-      "Warning, this command deletes packer caches and causes a re-install of doom-nvim on next launch.\n\n :DoomNuke `plugins`|`cache`|`all`. \n\t `cache` - Clear packer_compiled.lua\n\t `plugins` - Clear all installed plugins\n\t `all` - Delete all of the above."
+      "Warning, this command deletes packer caches and causes a re-install of doom-nvim on next launch.\n\n :DoomNuke plugins|cache|mason|all. \n\t `cache` - Clear packer_compiled.lua\n\t `plugins` - Clear all installed plugins\n\t `mason` - Clear all Mason.nvim packages\n\t `all` - Delete all of the above."
     )
     return
   end
@@ -275,6 +274,13 @@ functions.nuke = function(target)
     local plugin_dir = util.join_paths(vim.fn.stdpath("data"), "site", "pack")
     fs.rm_dir(plugin_dir)
     log.info("DoomNuke: Deleting packer plugins.  Doom-nvim will re-install on next launch.")
+  end
+
+  if target == "all" or target == "mason" then
+    local util = require("packer.util")
+    local mason_dir = util.join_paths(vim.fn.stdpath("data"), "mason")
+    fs.rm_dir(mason_dir)
+    log.info("DoomNuke: Deleting mason packages")
   end
 end
 
