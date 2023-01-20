5470f78709a967287227b2f5df972620dca36d01
Tuan Bui <tuanba1@gmail.com>
2023-01-20 12:04:11 +0700

    fix(modules,explorer):  Toggle show git ignored broken (#418)
    * fix: nvim-tree broken when lsp disabled
    
    * fix(explorer): wrong call back for gitignore

--- lua/doom/modules/features/explorer/init.lua
+++ lua/doom/modules/features/explorer/init.lua
@@ -139,7 +139,7 @@ explorer.configs["nvim-tree.lua"] = function()
           { key = "<BS>", cb = tree_cb("close_node") },
           { key = "<S-CR>", cb = tree_cb("close_node") },
           { key = "<Tab>", cb = tree_cb("preview") },
-          { key = "I", cb = tree_cb("toggle_ignored") },
+          { key = "I", cb = tree_cb("toggle_git_ignored") },
           { key = "H", cb = tree_cb("toggle_dotfiles") },
           { key = "R", cb = tree_cb("refresh") },
           { key = "a", cb = tree_cb("create") },
