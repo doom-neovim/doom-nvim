668f0c8ca687ef7afaf857c0496c016b6aaaa199
Tuan Bui <tuanba1@gmail.com>
2023-02-02 11:37:39 +0700

    Change formater and lsp for markdown

--- lua/doom/modules/langs/markdown/init.lua
+++ lua/doom/modules/langs/markdown/init.lua
@@ -13,7 +13,8 @@ markdown.settings = {
   disable_lsp = false,
   --- Name of the language server
   --- @type string
-  lsp_name = "remark_ls",
+  -- lsp_name = "remark_ls",
+  lsp_name = "marksman",
   --- Custom config to pass to nvim-lspconfig
   --- @type table|nil
   lsp_config = nil,
@@ -30,6 +31,25 @@ markdown.settings = {
   --- Function to configure null-ls diagnostics
   --- @type function|nil
   diagnostics_config = nil,
+
+  --- Disables null-ls formatting sources
+  --- @type boolean
+  disable_formatting = false,
+  --- Mason.nvim package to auto install the formatter from
+  --- @type string
+  formatting_package = "prettierd",
+  -- formatting_package = "prettier",
+  -- formatting_package = "eslint_d",
+  -- formatting_package = "eslint-lsp",
+  --- String to access the null_ls diagnositcs provider
+  --- @type string
+  -- formatting_provider = "builtins.formatting.eslint",
+  -- formatting_provider = "builtins.formatting.eslint_d",
+  -- formatting_provider = "builtins.formatting.prettier",
+  formatting_provider = "builtins.formatting.prettierd",
+  --- Function to configure null-ls formatter
+  --- @type function|nil
+  formatting_config = nil,
 }
 
 local langs_utils = require("doom.modules.langs.utils")
@@ -55,9 +75,53 @@ markdown.autocmds = {
           markdown.settings.diagnostics_config
         )
       end
+
+      if not markdown.settings.disable_formatting then
+        langs_utils.use_null_ls(
+          markdown.settings.formatting_package,
+          markdown.settings.formatting_provider,
+          markdown.settings.formatting_config
+        )
+      end
     end),
     once = true,
   },
 }
+
+markdown.packages = {
+  ["markdown-preview.nvim"] = {
+    "iamcco/markdown-preview.nvim",
+  },
+  ["clipboard-image.nvim"] = {
+    "ekickx/clipboard-image.nvim",
+  },
+}
+
+
+markdown.configs = {
+  ["markdown-preview.nvim"] = function()
+    vim.fn['mkdp#util#install']()
+  end,
+  ["clipboard-image.nvim"] = function()
+        require'clipboard-image'.setup {
+          default = {
+            img_name = function () -- ask for the image name
+              vim.fn.inputsave()
+              local name = vim.fn.input('Img Name: ')
+              vim.fn.inputrestore()
+
+              if name == nil or name == '' then
+                return os.date('%y-%m-%d-%H-%M-%S')
+              end
+              return name
+            end,
+           img_dir = {"%:p:h", "img"}  -- the directory to save the image which is relative to the current file
+          }
+        }
+
+  end,
+
+}
+
 
 return markdown
