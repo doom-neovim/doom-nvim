8a59ab14eb3b3c06227c7d172f4307aa6bd99492
Tuan Bui <tuanba1@gmail.com>
2023-04-06 11:50:45 +0700

    disable groups in Bufferline

--- lua/doom/modules/features/tabline/init.lua
+++ lua/doom/modules/features/tabline/init.lua
@@ -31,27 +31,27 @@ tabline.settings = {
         return true
       end
     end,
-    groups = {
-      options = {
-        toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-      },
-      items = {
-        {
-          name = "Tests",
-          auto_close = true,
-          matcher = function(buf)
-            return buf.filename:match("%_test") or buf.filename:match("%_spec")
-          end,
-        },
-        {
-          name = "Docs",
-          auto_close = true,
-          matcher = function(buf)
-            return buf.filename:match("%.md") or buf.filename:match("%.txt")
-          end,
-        },
-      },
-    },
+    -- groups = {
+    --   options = {
+    --     toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+    --   },
+    --   items = {
+    --     {
+    --       name = "Tests",
+    --       auto_close = true,
+    --       matcher = function(buf)
+    --         return buf.filename:match("%_test") or buf.filename:match("%_spec")
+    --       end,
+    --     },
+    --     {
+    --       name = "Docs",
+    --       auto_close = true,
+    --       matcher = function(buf)
+    --         return buf.filename:match("%.md") or buf.filename:match("%.txt")
+    --       end,
+    --     },
+    --   },
+    -- },
     offsets = {
       {
         filetype = "NvimTree",
