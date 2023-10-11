476a549c6ab98e79e07cf90c4ccdd35d4568e4e3
k <k@k.com>
2023-10-11 13:51:37 -0700

    fix annoying lua check errors diagnostics in config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,15 @@
 -- Just override stuff in the `doom` global table (it's injected into scope
 -- automatically).
 
+-- Fix annoying lua check warnings
+doom.modules.langs.lua.settings.lsp_config.settings.Lua.diagnostics = {
+  globals = {"vim", "doom"},
+  disable = {"missing-fields", "incomplete-signature-doc"},
+}
+
+vim.g.python3_host_prog = "/home/k/mambaforge/envs/python311/bin/python3"
+vim.o.linebreak = true
+
 -- ADDING A PACKAGE
 --
 -- doom.use_package("EdenEast/nightfox.nvim", "sainnhe/sonokai")
@@ -16,7 +25,101 @@ doom.use_package({
 
 doom.use_package("averms/black-nvim")
 
-doom.use_package({ "dccsillag/magma-nvim", run = ":UpdateRemotePlugins" })
+doom.use_package("luk400/vim-jukit")
+
+doom.use_keybind({
+  {
+    "<leader>o",
+    {
+      { "s",  "<NOP>", name = "jukit#splits#output" },
+      { "hs", "<NOP>", name = "jukit#splits#output_and_history" },
+      { "d",  "<NOP>", name = "jukit#splits#close_output_split" },
+      { "hd", "<NOP>", name = "jukit#splits#close_output_and_history" },
+    },
+  },
+  { "<leader>t", {
+    { "s", "<NOP>", name = "jukit#splits#term" },
+  } },
+  {
+    "<leader>h",
+    {
+      { "s", "<NOP>", name = "jukit#splits#history" },
+      { "d", "<NOP>", name = "jukit#splits#close_history" },
+      { "t", "<NOP>", name = "jukit#convert#save_nb_to_file" },
+    },
+  },
+  {
+    "<leader>s",
+    {
+      { "o", "<NOP>", name = "jukit#splits#show_last_cell_output" },
+      { "l", "<NOP>", name = "jukit#layouts#set_layout" },
+    },
+  },
+  -- { "<leader>j", {
+  --   { "", "<NOP>", name = "jukit#splits#out_hist_scroll" },
+  -- } },
+  -- { "<leader>k", {
+  --   { "", "<NOP>", name = "jukit#splits#out_hist_scroll" },
+  -- } },
+  { "<leader>a", {
+    { "h", "<NOP>", name = "jukit#splits#toggle_auto_hist" },
+  } },
+  {
+    "<leader>c",
+    {
+      { "o", "<NOP>", name = "jukit#cells#create_below" },
+      { "O", "<NOP>", name = "jukit#cells#create_above" },
+      { "t", "<NOP>", name = "jukit#cells#create_below markdown" },
+      { "T", "<NOP>", name = "jukit#cells#create_above markdown" },
+      { "d", "<NOP>", name = "jukit#cells#delete" },
+      { "s", "<NOP>", name = "jukit#cells#split" },
+      { "M", "<NOP>", name = "jukit#cells#merge_above" },
+      { "m", "<NOP>", name = "jukit#cells#merge_below" },
+      { "k", "<NOP>", name = "jukit#cells#move_up" },
+      { "j", "<NOP>", name = "jukit#cells#move_down" },
+    },
+  },
+  { "<leader>j", {
+    { "", "call jukit#cells#jump_to_next_cel()<CR>", name = "jukit#cells#jump_to_next_cell" },
+  } },
+  { "<leader>k", {
+    { "", "call jukit#cells#jump_to_previous_cell()<CR>", name = "jukit#cells#jump_to_previous_cell" },
+  } },
+  {
+    "<leader>d",
+    {
+      { "do", "<NOP>", name = "jukit#cells#delete_outputs" },
+      { "da", "<NOP>", name = "jukit#cells#delete_outputs" },
+    },
+  },
+  { "<leader>n", {
+    { "p", "<NOP>", name = "jukit#convert#notebook_convert" },
+  } },
+  -- {
+  --   "<leader>r",
+  --   {
+  --     { "ht", "<NOP>", name = "jukit#convert#save_nb_to_file" },
+  --     { "pd", "<NOP>", name = "jukit#convert#save_nb_to_file" },
+  --   },
+  -- },
+  -- { "<leader>p", {
+  --   { "d", "<NOP>", name = "jukit#convert#save_nb_to_file" },
+  -- } },
+})
+doom.use_keybind({
+  { "<leader>", {
+    { "<space>", "<NOP>", name = "jukit#send#section" },
+  } },
+  { "<cr>", {
+    { "", "<NOP>", name = "jukit#send#line" },
+  } },
+  -- { "<leader>c", {
+  --   { "c", "<NOP>", name = "jukit#send#until_current_section" },
+  -- } },
+  { "<leader>a", {
+    { "ll", "<NOP>", name = "jukit#send#all" },
+  } },
+})
 
 doom.use_package({
   "nvim-orgmode/orgmode",
@@ -27,13 +130,23 @@ doom.use_package({
       mappings = {
         org = {
           org_todo = { "t", "cit" },
-          org_insert_heading_respect_content = { "<Enter>", "<Leader>oih" },
+          org_insert_heading_respect_content = { "<S-CR>", "<Leader>oih" },
           org_cycle = { "<TAB>", "za" },
           org_global_cycle = { "<S-TAB>", "zA" },
         },
       },
     })
   end,
+})
+doom.use_keybind({
+  {
+    "<leader>o",
+    name = "+open/close",
+    {
+      { "a", "<NOP>", name = "Org Agenda" },
+      { "c", "<NOP>", name = "Org Capture" },
+    },
+  },
 })
 
 -- Orgmode: input mode tab and shift-tab to promote/demote subtree
@@ -67,6 +180,14 @@ doom.use_keybind({
   },
 })
 
+-- Sane jk behaviour
+doom.use_keybind({
+  {
+    { "j", "gj" },
+    { "k", "gk" },
+  },
+})
+
 doom.use_package({
   "github/copilot.vim",
   config = function()
@@ -81,7 +202,7 @@ doom.use_keybind({
       {
         options = { expr = true, script = true, replace_keycodes = false },
         {
-          { "<C-g>", 'copilot#Accept("<CR>")' },
+          { "<C-g>",  'copilot#Accept("<CR>")' },
           { "<C-CR>", 'copilot#Accept("<CR>")' },
         },
       },
@@ -89,6 +210,18 @@ doom.use_keybind({
       { "<C-k>", "<Plug>(copilot-previous)" },
       { "<C-o>", "<Plug>(copilot-dismiss)" },
       { "<C-f>", "<Plug>(copilot-suggest)" },
+    },
+  },
+})
+
+doom.use_keybind({
+  -- The `name` field will add the keybind to whichkey
+  {
+    "<leader>t",
+    name = "+tweak/toggle",
+    {
+      -- Bind to a vim command
+      { "w", "<cmd>set wrap!<CR>", name = "Toggle word wrap" },
     },
   },
 })
